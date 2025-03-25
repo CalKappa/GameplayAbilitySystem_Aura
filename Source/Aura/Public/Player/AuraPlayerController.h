62208aedd82cdd09e42079fcb921cedc9ff2dce5
Callum Thom <callum_thom@live.co.za>
2025-03-25 22:29:15 +0000

    Post Processing Highlight

--- Source/Aura/Public/Player/AuraPlayerController.h
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -10,6 +10,7 @@
 class UInputMappingContext;
 class UInputAction;
 struct FInputActionValue;
+class IEnemyInterface;
 
 
 /**
@@ -22,7 +23,7 @@ class AURA_API AAuraPlayerController : public APlayerController
 	
 public:
 	AAuraPlayerController();
-	
+	virtual void PlayerTick(float DeltaTime) override;
 
 
 protected:
@@ -38,5 +39,11 @@ private:
 	TObjectPtr<UInputAction> MoveAction;
 
 	void Move(const FInputActionValue& InputActionValue);
+
+	//Cursor Tracing
+	void CursorTrace();
+	TScriptInterface<IEnemyInterface> LastActor;
+	TScriptInterface<IEnemyInterface> ThisActor;
+
 
 };
