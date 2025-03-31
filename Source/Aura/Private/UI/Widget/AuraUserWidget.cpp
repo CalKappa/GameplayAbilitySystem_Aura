ecd5dbc458a8304894efff2ba16501a5f229f511
Callum Thom <callum_thom@live.co.za>
2025-03-31 20:20:10 +0100

    Health and Mana globes

--- Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -3,3 +3,11 @@
 
 #include "UI/Widget/AuraUserWidget.h"
 
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
+
+
+
--- Source/Aura/Public/UI/WidgetController/AuraWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/AuraWidgetController.h
@@ -6,6 +6,9 @@
 #include "UObject/NoExportTypes.h"
 #include "AuraWidgetController.generated.h"
 
+
+class UAttributeSet;
+class UAbilitySystemComponent;
 /**
  * 
  */
@@ -13,5 +16,26 @@ UCLASS()
 class AURA_API UAuraWidgetController : public UObject
 {
 	GENERATED_BODY()
+
+
+protected:
+
+	/** Widget Controller is interested in 4 main classes
+	*	1 - Ability system component
+	*	2 - Attribute set
+	*	3 - Player state
+	*	4 - Player controller
+	*/
+	UPROPERTY(BlueprintReadOnly, Category = "WidgetController")
+	TObjectPtr<APlayerController> PlayerController;
+	
+	UPROPERTY(BlueprintReadOnly, Category = "WidgetController")
+	TObjectPtr<APlayerState> PlayerState;
+
+	UPROPERTY(BlueprintReadOnly, Category = "WidgetController")
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+
+	UPROPERTY(BlueprintReadOnly, Category = "WidgetController")
+	TObjectPtr<UAttributeSet> AttributeSet;
 	
 };
