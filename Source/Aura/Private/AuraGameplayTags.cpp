ea67da24f0fcdd63d5854dc0c6b0e373d2d28636
Callum Thom <callum_thom@live.co.za>
2025-04-21 09:54:28 +0100

    Native Gameplay Tags

---
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -0,0 +1,45 @@
+// Copyright CalKappa
+
+
+#include "AuraGameplayTags.h"
+#include "GameplayTagsManager.h"
+
+
+FAuraGameplayTags FAuraGameplayTags::GameplayTags;
+
+void FAuraGameplayTags::InitializeNativeGameplayTags()
+{
+	// -------- PRIMARY ATTRIBUTES --------
+	GameplayTags.Attributes_Primary_Strength = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attributes.Primary.Strength"), FString("Increases Physical Damage"));
+	GameplayTags.Attributes_Primary_Intelligence = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attributes.Primary.Intelligence"), FString("Increases Magic Damage, Max Mana and Mana Regen"));
+	GameplayTags.Attributes_Primary_Resilience = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attributes.Primary.Resilience"), FString("Increases Armor and Armor Penetration"));
+	GameplayTags.Attributes_Primary_Vigor = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attributes.Primary.Vigor"), FString("Increases Max Health and Health Regeneration"));
+
+
+	// -------- SECONDARY ATTRIBUTES --------
+	GameplayTags.Attributes_Secondary_Armor = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attributes.Secondary.Armor"), FString("Reduces damage taken, improves Block Chance"));
+	GameplayTags.Attributes_Secondary_ArmorPenetration = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attributes.Secondary.ArmorPenetration"), FString("Reduces damage taken, improves Block Chance"));
+	GameplayTags.Attributes_Secondary_BlockChance = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attributes.Secondary.BlockChance"), FString("Reduces damage taken, improves Block Chance"));
+	GameplayTags.Attributes_Secondary_CriticalHitChance = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attributes.Secondary.Critical Hit Chance"), FString("Reduces damage taken, improves Block Chance"));
+	GameplayTags.Attributes_Secondary_CriticalHitDamage = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attributes.Secondary.CriticalHitDamage"), FString("Reduces damage taken, improves Block Chance"));
+	GameplayTags.Attributes_Secondary_CriticalHitResistance = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attributes.Secondary.CriticalHitResistance"), FString("Reduces damage taken, improves Block Chance"));
+	GameplayTags.Attributes_Secondary_HealthRegeneration = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attributes.Secondary.HealthRegeneration"), FString("Reduces damage taken, improves Block Chance"));
+	GameplayTags.Attributes_Secondary_ManaRegeneration = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attributes.Secondary.ManaRegeneration"), FString("Reduces damage taken, improves Block Chance"));
+	GameplayTags.Attributes_Secondary_MaxHealth = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attributes.Secondary.MaxHealth"), FString("Reduces damage taken, improves Block Chance"));
+	GameplayTags.Attributes_Secondary_MaxMana = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Attributes.Secondary.MaxMana"), FString("Reduces damage taken, improves Block Chance"));
+}
+
